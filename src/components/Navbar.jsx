1f4a92c7859e2ca5cd87a9c8ccb69fe27cc94607
Suhayb Jirde <dribblekayga@gmail.com>
2023-07-04 09:53:33 -0800

    add active class to the navbar to show it's active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = ()=> (
     <header className="header">
-        <h1 className="header__logo"><Link to='/'>#VANLIFE</Link></h1>
+        <h1 className="header__logo"><NavLink to='/'>#VANLIFE</NavLink></h1>
         <nav className="header__nav">
             <ul className="header__nav__ul">
-                <li className="header__nav__ul_link"><Link to='/about'>About</Link></li>
-                <li className="header__nav__ul_link"><Link to='/vans'>Vans</Link></li>
+                <li className="header__nav__ul_link"><NavLink to='/about' className={({isActive}) => isActive ? 'active' : null}>About</NavLink></li>
+                <li className="header__nav__ul_link"><NavLink to='/vans' className={({isActive}) => isActive ? 'active' : null}>Vans</NavLink></li>
             </ul>
         </nav>
     </header>
