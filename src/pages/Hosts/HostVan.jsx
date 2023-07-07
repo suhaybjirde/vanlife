2881eb49e0af72eb2702e510c1071ecd1d9778f1
Suhayb Jirde <dribblekayga@gmail.com>
2023-07-06 21:24:27 -0800

    host/vans page markup

--- src/pages/Hosts/HostVan.jsx
+++ src/pages/Hosts/HostVan.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const HostVan = ()=> {
+    const [data, setData] = useState(null)
+    
+    useEffect(()=> {
+        async function getData() {
+            const res = await fetch('/api/host/vans');
+            const actualData = await res.json()
+            setData(actualData.vans)
+        }
+        getData()
+    },[])
+    if (!data) return
+    const vans = data.map(van => (
+        <Link 
+            to={van.id} 
+            key={van.id}
+        >
+            <article>
+                <img src={van.imageUrl} alt="" width='100px'/>
+                <div>
+                    <h3>{van.name}</h3>
+                    <p>{van.price} <span>/day</span></p>
+                </div>
+            </article>
+        </Link>
+    ))
     return (
-        <h1>Vans Here</h1>
+        <section>
+            <h1>Your listed vans</h1>
+            {vans}
+        </section>
     )
 }
 
