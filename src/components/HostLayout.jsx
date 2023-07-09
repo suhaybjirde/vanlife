d3a8e489a2890295f4a5e224a52b13451444acac
Suhayb Jirde <dribblekayga@gmail.com>
2023-07-08 20:44:57 -0800

    style /Host Nav

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
 const HostNav = ()=> (
-    <nav>
-        <ul>
-            <li>
-                <NavLink to='.'>Dashboard</NavLink>
+    <nav className="host-nav">
+        <ul className="host-nav__list">
+            <li className="host-nav__list__item">
+                <NavLink to='.' end>Dashboard</NavLink>
             </li>
-            <li>
+            <li className="host-nav__list__item">
                 <NavLink to='income'>Income</NavLink>
             </li>
-            <li>
+            <li className="host-nav__list__item">
                 <NavLink to='vans'>Vans</NavLink>
             </li>
-            <li>
+            <li className="host-nav__list__item">
                 <NavLink to='reviews'>Reviews</NavLink>
             </li>
         </ul>
