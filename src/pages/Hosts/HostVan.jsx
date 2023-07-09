bd43d50aaf33bd1c7f57260605d73998b6aa2f72
Suhayb Jirde <dribblekayga@gmail.com>
2023-07-08 21:08:30 -0800

    style Host/Vans page

--- src/pages/Hosts/HostVan.jsx
+++ src/pages/Hosts/HostVan.jsx
@@ -17,19 +17,20 @@ const HostVan = ()=> {
         <Link 
             to={van.id} 
             key={van.id}
+            className="host-vans__item"
         >
-            <article>
-                <img src={van.imageUrl} alt="" width='100px'/>
-                <div>
-                    <h3>{van.name}</h3>
-                    <p>{van.price} <span>/day</span></p>
+            <article className="host-vans__item__van">
+                <img src={van.imageUrl} alt="" width='100px' className="host-vans__item__van__img"/>
+                <div className="host-vans__item__van__info">
+                    <h3 className="host-vans__item__van__info__name">{van.name}</h3>
+                    <p className="host-vans__item__van__info__price">${van.price}/day</p>
                 </div>
             </article>
         </Link>
     ))
     return (
-        <section>
-            <h1>Your listed vans</h1>
+        <section className="host-vans">
+            <h1 className="host-vans__title">Your listed vans</h1>
             {vans}
         </section>
     )
